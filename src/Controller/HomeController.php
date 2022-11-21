4c2066c6c9c3a7bbe19152bfe2f21335f11d87ae
aichawoury <wouryaichajiba@gmail.com>
2022-11-21 18:02:59 -0500

    premier page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,13 @@
 
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
+// use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController
 
 {
+    // @Route()
     public function test()
     { 
         return new Response("hello world");
